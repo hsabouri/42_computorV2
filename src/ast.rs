1ebd54a4f485705b8e4067ed5b4f7de77d171eb1
Hugo Sabourin <hsabouri@student.42.fr>
2018-06-10 16:50:13 +0200

    Prompt with line editing and history

--- src/ast.rs
+++ src/ast.rs
@@ -1,11 +1,41 @@
-#[derive(Debug, Copy, Clone)]
+use std::f32;
+use std::ops::{Add, Sub, Mul, Div, Rem};
+use std::fmt;
+
+#[derive(Copy, Clone)]
 pub enum Opcode {
     Add,
     Sub,
-    Mult,
+    Mul,
     Div,
     Rem,
     Pow,
+}
+
+impl fmt::Debug for Opcode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Opcode::Add => write!(f, "add"),
+            Opcode::Sub => write!(f, "substract"),
+            Opcode::Mul => write!(f, "multiply"),
+            Opcode::Div => write!(f, "divide"),
+            Opcode::Rem => write!(f, "modulo"),
+            Opcode::Pow => write!(f, "power"),
+        }
+    }
+}
+
+impl fmt::Display for Opcode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Opcode::Add => write!(f, "+"),
+            Opcode::Sub => write!(f, "-"),
+            Opcode::Mul => write!(f, "*"),
+            Opcode::Div => write!(f, "/"),
+            Opcode::Rem => write!(f, "%"),
+            Opcode::Pow => write!(f, "^"),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -14,7 +44,7 @@ pub enum Input {
     Eval(Box<Expr>)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Expr {
     Number(f32),
     Imaginary,
@@ -22,4 +52,116 @@ pub enum Expr {
     Variable(String),
     Function(String, Box<Expr>),
     Op(Box<Expr>, Opcode, Box<Expr>)
+}
+
+impl Expr {
+    fn operation_error(self, op: Opcode) -> String {
+        match self {
+            Expr::Number(_) => format!("Can't {:?} on numbers", op),
+            Expr::Imaginary => format!("Can't {:?} on complex numbers", op),
+            Expr::Matrix(_) => format!("Can't {:?} on matrices", op),
+            Expr::Variable(_) => format!("Can't {:?} on raw variables", op),
+            Expr::Function(_, _) => format!("Can't {:?} on raw functions", op),
+            Expr::Op(_, _, _) => format!("Can't {:?} on raw exprs", op),
+        }
+    }
+
+    pub fn pow(self, other: Expr) -> Result<Expr, String> {
+        match self {
+            Expr::Number(a) => match other {
+                Expr::Number(b) => Ok(Expr::Number(a.powf(b))),
+                b => Err(b.operation_error(Opcode::Add))
+            },
+            a => Err(a.operation_error(Opcode::Add))
+        }
+    }
+}
+
+impl Add for Expr {
+    type Output = Result<Expr, String>;
+
+    fn add(self, other: Expr) -> Result<Expr, String> {
+        match self {
+            Expr::Number(a) => match other {
+                Expr::Number(b) => Ok(Expr::Number(a + b)),
+                b => Err(b.operation_error(Opcode::Add))
+            },
+            a => Err(a.operation_error(Opcode::Add))
+        }
+    }
+}
+
+impl Sub for Expr {
+    type Output = Result<Expr, String>;
+
+    fn sub(self, other: Expr) -> Result<Expr, String> {
+        match self {
+            Expr::Number(a) => match other {
+                Expr::Number(b) => Ok(Expr::Number(a - b)),
+                b => Err(b.operation_error(Opcode::Sub))
+            },
+            a => Err(a.operation_error(Opcode::Sub))
+        }
+    }
+}
+
+impl Mul for Expr {
+    type Output = Result<Expr, String>;
+
+    fn mul(self, other: Expr) -> Result<Expr, String> {
+        match self {
+            Expr::Number(a) => match other {
+                Expr::Number(b) => Ok(Expr::Number(a * b)),
+                b => Err(b.operation_error(Opcode::Mul))
+            },
+            a => Err(a.operation_error(Opcode::Mul))
+        }
+    }
+}
+
+impl Div for Expr {
+    type Output = Result<Expr, String>;
+
+    fn div(self, other: Expr) -> Result<Expr, String> {
+        match self {
+            Expr::Number(a) => match other {
+                Expr::Number(b)
+                    if b >= 0.0 + f32::EPSILON &&
+                    b <= 0.0 - f32::EPSILON => Ok(Expr::Number(a / b)),
+                b => Err(b.operation_error(Opcode::Div))
+            },
+            a => Err(a.operation_error(Opcode::Div))
+        }
+    }
+}
+
+impl Rem for Expr {
+    type Output = Result<Expr, String>;
+
+    fn rem(self, other: Expr) -> Result<Expr, String> {
+        match self {
+            Expr::Number(a) => match other {
+                Expr::Number(b) if b >= 0.0 + f32::EPSILON || b <= 0.0 - f32::EPSILON => {
+                    let (a, b) = (a as i32, b as i32);
+
+                    Ok(Expr::Number((a % b) as f32))
+                },
+                b => Err(b.operation_error(Opcode::Rem))
+            },
+            a => Err(a.operation_error(Opcode::Rem))
+        }
+    }
+}
+
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expr::Number(ref a) => write!(f, "{}", a),
+            Expr::Imaginary => write!(f, "i"),
+            Expr::Matrix(_) => write!(f, "[...]"),
+            Expr::Variable(ref s) => write!(f, "{}", s),
+            Expr::Function(ref s, ref e) => write!(f, "{}({})", s, *e),
+            Expr::Op(ref a, ref o, ref b) => write!(f, "{} {} {}", a, o, b)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,94 @@
+extern crate rustyline;
+
 mod parser;
 mod ast;
 
-use ast::*;
+use std::collections::HashMap;
+use ast::{Expr, Input, Opcode};
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+fn solve(vars: &HashMap<String, Expr>, expr: Expr) -> Result<Expr, String> {
+    let expr2 = expr.clone();
+    let res = match expr {
+        Expr::Number(_) => Ok(expr),
+        Expr::Imaginary => Ok(expr),
+        Expr::Matrix(_) => Ok(expr), //TODO
+        Expr::Variable(s) => match vars.get(&s.to_lowercase()).cloned() {
+            Some(value) => Ok(value),
+            None => return Err(format!("Error: Variable '{}' is undefined.", s)),
+        },
+        Expr::Function(_, expr) => Ok(*expr), // TODO,
+        Expr::Op(a, op, b) => match op {
+            Opcode::Add => solve(vars, *a)? + solve(vars, *b)?,
+            Opcode::Mul => solve(vars, *a)? * solve(vars, *b)?,
+            Opcode::Sub => solve(vars, *a)? - solve(vars, *b)?,
+            Opcode::Div => solve(vars, *a)? / solve(vars, *b)?,
+            Opcode::Rem => solve(vars, *a)? % solve(vars, *b)?,
+            Opcode::Pow => solve(vars, *a)?.pow(solve(vars, *b)?),
+        },
+    };
+    match expr2 {
+        Expr::Number(_) => print!(""),
+        _ => println!("{} => {}", expr2, res.clone().unwrap())
+    }
+    res
+}
+
+fn assign(vars: &mut HashMap<String, Expr>, var: Expr, expr: Expr) -> Result<Expr, String> {
+    let res = solve(&vars, expr)?;
+    let var_str = match var {
+        Expr::Variable(var_str) => var_str.to_lowercase(),
+        _ => return Err(format!("Can only assign to variables")),
+    };
+
+    vars.insert(var_str, res.clone());
+    Ok(res)
+}
+
+fn eval(vars: &HashMap<String, Expr>, expr: Expr) -> Result<Expr, String> {
+   solve(&vars, expr)
+}
+
+fn parse(vars: &mut HashMap<String, Expr>, line: String) {
+    match parser::EquExprParser::new().parse(&line) {
+        Ok(Input::Assignation(left, right)) => match assign(vars, *left, *right) {
+            Ok(expr) => println!("{}", expr),
+            Err(err) => println!("{}", err)
+        },
+        Ok(Input::Eval(expr)) => match eval(&vars, *expr) {
+            Ok(expr) => println!("{}", expr),
+            Err(err) => println!("{}", err)
+        },
+        Err(err) => println!("{}", err),
+    };
+}
 
 fn main() {
-    let expr = parser::EquExprParser::new()
-        .parse("func(x) = 3 * x + 1 - kjsd ^ 18 * 2");
-    println!("{:#?}", expr);
+    let mut reader = Editor::<()>::new();
+    let mut vars: HashMap<String, Expr> = HashMap::new();
+
+    reader.load_history("computor_history");
+    loop {
+        match reader.readline("computor > ") {
+            Ok(line) => {
+                if line.clone() == format!("exit") {break;}
+                reader.add_history_entry(line.as_ref());
+                parse(&mut vars, line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("Ctrl-C");
+                break;
+            },
+            Err(ReadlineError::Eof) => {
+                println!("Ctrl-D");
+                break;
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    reader.save_history("computor_history").unwrap();
 }
