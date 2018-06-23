7516d85c20f7954cde2ae72b2c327322daa5f045
Hugo Sabourin <hsabouri@student.42.fr>
2018-06-23 19:16:01 +0200

    Inverting 2x2 matrices, dividing matrices

--- src/proddiv_trait.rs
+++ src/proddiv_trait.rs
@@ -1,13 +1,12 @@
 use ast::{Prod, ProdDiv, Expr, Opcode};
 use std::f32;
 
-/*
 fn matrice_invert(mat: Vec<Vec<Box<Expr>>>) -> Result<Expr, String> {
     let invert = match (mat.len(), mat[0].len()) {
         (a, b) if a != b =>
             return Err(format!("non-square matrice [{}, {}]", a, b)),
-        (2, 2) => match ((*mat[0][0] * *mat[1][1])? - (*mat[0][1] * *mat[1][0])?)? { 
-            det if det >= 0.0 - f32::EPSILON && det >= 0.0 - f32::EPSILON =>
+        (2, 2) => match ((*mat[0][0].clone() * *mat[1][1].clone())? - (*mat[0][1].clone() * *mat[1][0].clone())?)? { 
+            ref det if det >= &Expr::Number(0.0 - f32::EPSILON) && det >= &Expr::Number(0.0 - f32::EPSILON) =>
                 return Err(format!("det({}) == 0", Expr::Matrix(mat))),
             det => (Expr::Number(1.0) / det)? * Expr::Matrix(mat) // Inverting 2x2 matrices
         },
@@ -18,12 +17,14 @@ fn matrice_invert(mat: Vec<Vec<Box<Expr>>>) -> Result<Expr, String> {
         Err(s) => Err(format!("can't invert: {}", s))
     }
 }    
-*/
 
 impl ProdDiv for Expr {
     type Output = Result<Expr, String>;
 
     fn prod_div(self, other: Expr) -> Result<Expr, String> {
-        Err(format!("Implement the damn productDivision trait"))
+        match (self, other) {
+            (a, Expr::Matrix(b)) => (a * matrice_invert(b)?),
+            (a, b) => Err(Expr::type_error(a, b, Opcode::ProdDiv)),
+        }
     }
 }
