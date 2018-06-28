c23b81c4bd50f8c5ec0943b522b6879a64fe0004
Hugo Sabourin <hsabouri@student.42.fr>
2018-06-28 17:28:09 +0200

    cleaned solver

--- src/solver.rs
+++ src/solver.rs
@@ -4,12 +4,14 @@ use ast::*;
 
 pub struct Solver {
     vars: HashMap<String, Expr>,
+    funcs: HashMap<String, (String, Expr)>
 }
 
 impl Solver {
     pub fn new() -> Solver {
         Solver {
             vars: HashMap::<String, Expr>::new(),
+            funcs: HashMap::<String, (String, Expr)>::new(),
         }
     }
 
@@ -52,23 +54,42 @@ impl Solver {
         }
     }
 
+    fn solve_function(&self, expr: Expr, arg_name: String, arg_value: Expr) -> Result<Expr, String> {
+        Err(format!("Implement the god damn function solver"))
+    }
+
+    fn handle_complex(&self, a: f32, b: f32) -> Result<Expr, String> {
+        if b < 0.0 + f32::EPSILON && b > 0.0 - f32::EPSILON {
+            Ok(Expr::Number(a))
+        } else {
+            Ok(Expr::Complex(a, b))
+        }
+    }
+
+    fn handle_variable(&self, s: String) -> Result<Expr, String> {
+        match self.vars.get(&s.to_lowercase()).cloned() {
+            Some(value) => Ok(value),
+            None => Err(format!("Error: Variable '{}' is undefined.", s)),
+        }
+    }
+
+    fn handle_function(&self, s: String, expr: Expr) -> Result<Expr, String> {
+        match self.funcs.get(&s.to_lowercase()).cloned() {
+            Some((arg_name, func_expr)) => {
+                self.solve_function(func_expr, arg_name, expr)
+            },
+            None => Err(format!("Error: Function '{}' is undefined.", s))
+        }
+    }
+
     fn solve(&self, expr: Expr) -> Result<Expr, String> {
         match expr {
-            Expr::Number(_) => Ok(expr),
-            Expr::Imaginary => Ok(expr),
-            Expr::Complex(a, b) => {
-                if b < 0.0 + f32::EPSILON && b > 0.0 - f32::EPSILON {
-                    Ok(Expr::Number(a))
-                } else {
-                    Ok(expr)
-                }
-            },
+            Expr::Number(a) => Ok(Expr::Number(a)),
+            Expr::Imaginary => Ok(Expr::Imaginary),
+            Expr::Complex(a, b) => self.handle_complex(a, b),
             Expr::Matrix(matrice) => self.reduce_matrice(matrice),
-            Expr::Variable(s) => match self.vars.get(&s.to_lowercase()).cloned() {
-                Some(value) => Ok(value),
-                None => return Err(format!("Error: Variable '{}' is undefined.", s)),
-            },
-            Expr::Function(_, expr) => Ok(*expr), // TODO,
+            Expr::Variable(s) => self.handle_variable(s),
+            Expr::Function(s, expr) => self.handle_function(s, *expr),
             Expr::Op(a, op, b) => match op {
                 Opcode::Add => self.solve(*a)? + self.solve(*b)?,
                 Opcode::Mul => self.solve(*a)? * self.solve(*b)?,
